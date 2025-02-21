acef435d35756077dd28b9af3d533d28a5fb7b15
Stan <stanleycherry42+github@gmail.com>
2025-02-21 16:34:33 +0100

    disable some tests

--- src/math.zig
+++ src/math.zig
@@ -1,9 +1,8 @@
 const std = @import("std");
-pub const matrix = @import("matrix.zig");
+// pub const matrix = @import("matrix.zig");
 pub const vector = @import("vector.zig");
 pub const ortho = @import("ortho.zig");
-pub const collision = @import("collision.zig");
-pub const approx = @import("approx.zig");
+// pub const collision = @import("collision.zig");
 
 test {
     @import("tests").init();
