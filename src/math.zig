73cb08a95a0a193bbf4cf98ea3ca2c2f772b41c5
Stan <stanleycherry42+github@gmail.com>
2025-03-08 17:19:52 +0100

    add randFloatRange function

--- src/math.zig
+++ src/math.zig
@@ -1,3 +1,8 @@
 const std = @import("std");
 pub const matrix = @import("math/matrix.zig");
 pub const vector = @import("math/vector.zig");
+
+pub fn randFloatRange(T: type, from: T, to: T, rand: std.Random) T {
+    std.debug.assert(from <= to);
+    return from + (to - from) * rand.float(T);
+}
