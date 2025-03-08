e63eb2f932cc9fac8fc5e9f12eedeff9d7dc4b56
Stan <stanleycherry42+github@gmail.com>
2025-03-08 17:22:58 +0100

    add init() function in tests.zig

--- src/stanlib.zig
+++ src/stanlib.zig
@@ -6,11 +6,6 @@ const std = @import("std");
 const tests = @import("tests");
 
 test {
-    tests.PRNG = std.Random.DefaultPrng.init(std.crypto.random.int(u64));
-    tests.RAND = tests.PRNG.random();
-    std.testing.refAllDeclsRecursive(struct {
-        pub const math = @import("math.zig");
-        // pub const ortho = @import("ortho.zig");
-        pub const collision = @import("collision.zig");
-    });
+    tests.init();
+    _ = collision.portal;
 }
--- src/tests.zig
+++ src/tests.zig
@@ -1,4 +1,11 @@
 const std = @import("std");
 
+const stanlib = @import("stanlib");
+
 pub var PRNG: std.Random.DefaultPrng = undefined;
 pub var RAND: std.Random = undefined;
+
+pub fn init() void {
+    PRNG = std.Random.DefaultPrng.init(std.crypto.random.int(u64));
+    RAND = PRNG.random();
+}
