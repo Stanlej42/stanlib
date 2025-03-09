d036b937206bb08664b114d3b1c6301898340952
Stan <stanleycherry42+github@gmail.com>
2025-03-09 16:51:06 +0100

    add functions for generating random AABBs

--- src/collision/AABB.zig
+++ src/collision/AABB.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const tests = @import("tests");
 const vector = @import("stanlib").math.vector;
 
 pub fn AABB(comptime N: comptime_int, comptime T: type) type {
@@ -46,14 +47,73 @@ pub fn AABB(comptime N: comptime_int, comptime T: type) type {
     };
 }
 
-test {
-    const N = 2;
+pub fn randomIntersecting(N: comptime_int, T: type, rand: std.Random) [2]AABB(N, T) {
+    var points: [4]@Vector(N, T) = undefined;
+    vector.fillRandomRange(N, T, &points, rand, -1, 1);
+    for (0..3) |i| {
+        for (0..3 - i) |j| {
+            const minp = @min(points[j], points[j + 1]);
+            const maxp = @max(points[j], points[j + 1]);
+            points[j] = minp;
+            points[j + 1] = maxp;
+        }
+    }
+    var aabbs: [2]AABB(N, T) = undefined;
+    aabbs[0] = AABB(N, T).new(points[0], points[2]);
+    aabbs[1] = AABB(N, T).new(points[1], points[3]);
+    return aabbs;
+}
+
+test "intersecting" {
+    const N = 10;
     const T = f32;
-    const aabb1 = AABB(N, T).new(.{ -1.0, -1.0 }, .{ 1.0, 1.0 });
-    const aabb2 = AABB(N, T).new(.{ 0.0, 0.0 }, .{ 2.0, 2.0 });
-    try std.testing.expect(aabb1.intersects(aabb2));
-    try std.testing.expect(aabb2.intersects(aabb1));
-    try std.testing.expectEqual(@Vector(N, T){ 0.0, 0.0 }, aabb1.interior());
-    try std.testing.expectEqual(@Vector(N, T){ 1.0, 1.0 }, aabb2.interior());
-    try std.testing.expectEqual(@Vector(N, T){ 0.0, 2.0 }, aabb2.support(.{ -1.0, 1.0 }));
+    for (0..100000) |_| {
+        const aabbs = randomIntersecting(N, T, tests.RAND);
+        try std.testing.expect(aabbs[0].intersects(aabbs[1]));
+    }
+}
+
+pub fn randomDisjoint(N: comptime_int, T: type, rand: std.Random) [2]AABB(N, T) {
+    var points: [4]@Vector(N, T) = undefined;
+    vector.fillRandomRange(N, T, &points, rand, -1, 1);
+    var aabbs: [2]AABB(N, T) = undefined;
+    aabbs[0] = AABB(N, T).new(
+        @min(points[0], points[1]),
+        @max(points[0], points[1]),
+    );
+    aabbs[1] = AABB(N, T).new(
+        @min(points[2], points[3]),
+        @max(points[2], points[3]),
+    );
+    const coord_i: usize = rand.intRangeLessThan(usize, 0, N);
+    var coords: [4]T = .{
+        points[0][coord_i],
+        points[1][coord_i],
+        points[2][coord_i],
+        points[3][coord_i],
+    };
+    std.mem.sort(T, &coords, {}, std.sort.asc(T));
+    const aabbs_i: usize = rand.intRangeLessThan(usize, 0, 2);
+    aabbs[aabbs_i].bottom_left[coord_i] = coords[0];
+    aabbs[aabbs_i].top_right[coord_i] = coords[1];
+    aabbs[1 - aabbs_i].bottom_left[coord_i] = coords[2];
+    aabbs[1 - aabbs_i].top_right[coord_i] = coords[3];
+    return aabbs;
+}
+
+test "disjoint" {
+    const N = 10;
+    const T = f32;
+    for (0..100000) |_| {
+        const aabbs = randomDisjoint(N, T, tests.RAND);
+        try std.testing.expect(!aabbs[0].intersects(aabbs[1]));
+    }
+}
+
+pub fn randomAABBs(N: comptime_int, T: type, rand: std.Random) [2]AABB(N, T) {
+    if (rand.boolean()) {
+        return randomIntersecting(N, T, rand);
+    } else {
+        return randomDisjoint(N, T, rand);
+    }
 }
